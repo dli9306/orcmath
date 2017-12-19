656bc85748326cf0155bfe1882e3fb9d4d64f98b
David <David@192.168.0.7>
2017-12-18 21:36:33 -0500

    x

--- OrcMathGUI/src/playground/CatalogMaker.java
+++ OrcMathGUI/src/playground/CatalogMaker.java
@@ -1,11 +1,13 @@
 package playground;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import guiPlayer.Sampler;
 
 public class CatalogMaker {
     static ArrayList<Catalog> booklist;
+    
 	 
 	public CatalogMaker() {
 		booklist =  new ArrayList<Catalog>();
@@ -16,8 +18,43 @@ public class CatalogMaker {
 	}
 	public static void main(String[] args){
 		CatalogMaker c = new CatalogMaker();
+		newEntry();
+		Scanner n = new Scanner(System.in);
+		System.out.println("Would you like to save this file?");
+		String s = n.nextLine();
+		if(s.toLowerCase().contains("yes")) {
+			
+		}
+		
 		System.out.println(c.getCSContent());
 	}
+	public static void newEntry() {
+		int page= 0;
+		String title = "";
+		String author = "";
+		Scanner n = new Scanner(System.in);
+		System.out.println("Name of book");
+		String s = n.nextLine();
+		title = s;
+		System.out.println("Name of author");
+		String a = n.nextLine();
+		author = a;
+		System.out.println("Number of pages");
+		String p = n.nextLine();
+	     page = Integer.parseInt(p);
+		booklist.add(new Catalog(s,a,page));
+		System.out.println("Would you like to make another entry?");
+		String x = n.nextLine();
+		if(x.toLowerCase().contains("yes")){
+			newEntry();
+		}
+		else
+		{
+			System.out.println("Done");
+		}
+		
+	}
+
 	public String getCSContent() {
 		String data = "";
 		for(Catalog e:booklist) {
