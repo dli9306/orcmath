7ac6b4f821aaa753a879902ea1362e232565ec37
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-12-20 11:07:34 -0500

    x

--- OrcMathGUI/src/playground/CatalogScreen.java
+++ OrcMathGUI/src/playground/CatalogScreen.java
@@ -1,14 +1,23 @@
 package playground;
 
+import java.io.File;
 import java.util.List;
 
+import javax.swing.JFrame;
+
 import guiTeacher.components.*;
+import guiTeacher.interfaces.FileRequester;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
-public class CatalogScreen extends FullFunctionScreen {
+public class CatalogScreen extends FullFunctionScreen implements FileRequester {
 	
 	private TextField descriptionField; 
+	private TextArea text;
+	private Button button;
+	private FileOpenButton openButton;
+	private CatalogMaker catalog;
+	
 
 	public CatalogScreen(int width, int height) {
 		super(width, height);
@@ -19,14 +28,75 @@ public class CatalogScreen extends FullFunctionScreen {
 	public void initAllObjects(List<Visible> viewObjects) {
 		//text field: single line of text
 		//text box: multi-line
+		catalog = new CatalogMaker();
 		descriptionField = new TextField(40, 40, 200, 30, "Title!","Name of Book");
 		viewObjects.add(descriptionField);
 		descriptionField = new TextField(40, 100, 200, 30, "Author!","Name of Author");
 		viewObjects.add(descriptionField);
 		descriptionField = new TextField(40, 160, 200, 30, "Pages!","Number of Pages");
 		viewObjects.add(descriptionField);
+		text = new TextArea(40, 240, 200, 100, "PUT TEXT HERE");
+		viewObjects.add(text);
+		button = new Button(40,300,100,40,"Add Button",new Action() {
+			
+			@Override
+			public void act() {
+				addClicked();
+				
+			}
+		});
+		viewObjects.add(button);
+		button = new Button(150,300,100,40,"Save Button",new Action() {
+			
+			@Override
+			public void act() {
+				saveClicked();
+				
+			}
+		});
+		viewObjects.add(button);
+		button = new Button(270,300,100,40,"Delete Button",new Action() {
+			
+			@Override
+			public void act() {
+				deleteclicked();
+				
+				
+			}
 
+		});
+		viewObjects.add(button);
+		openButton = new FileOpenButton(380, 300, 100, 40, null, this);
+		viewObjects.add(openButton);
+       
 
+	}
+
+	protected void deleteclicked() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void saveClicked() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void addClicked() {
+		text.setText("You clicked the button...");
+		
+	}
+
+	@Override
+	public void setFile(File f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JFrame getWindow() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
