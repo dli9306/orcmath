83fcbc5b3524bcbd64f428eb8fc2c12aede58c8c
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-12-20 10:33:28 -0500

    x

--- OrcMathGUI/src/playground/CatalogScreen.java
+++ OrcMathGUI/src/playground/CatalogScreen.java
@@ -2,7 +2,7 @@ package playground;
 
 import java.util.List;
 
-import guiTeacher.components.TextField;
+import guiTeacher.components.*;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
@@ -17,12 +17,15 @@ public class CatalogScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
+		//text field: single line of text
+		//text box: multi-line
 		descriptionField = new TextField(40, 40, 200, 30, "Title!","Name of Book");
 		viewObjects.add(descriptionField);
 		descriptionField = new TextField(40, 100, 200, 30, "Author!","Name of Author");
 		viewObjects.add(descriptionField);
 		descriptionField = new TextField(40, 160, 200, 30, "Pages!","Number of Pages");
 		viewObjects.add(descriptionField);
+
 
 	}
 
