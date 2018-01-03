0ac9bf3fbbb8deb0a7e21b8e1f12c59368e987f9
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-01-03 11:15:42 -0500

    x

--- OrcMathGUI/src/mystuff/SimonGameDavid.java
+++ OrcMathGUI/src/mystuff/SimonGameDavid.java
@@ -1,17 +1,26 @@
 package mystuff;
 
+import guiPlayer.CatalogMakerGUI;
+import guiPlayer.CatalogScreen;
+import guiPlayer.CustomGUI;
 import guiTeacher.GUIApplication;
 
 public class SimonGameDavid extends GUIApplication {
 
 	public SimonGameDavid(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		SimonGameDavid simon = new SimonGameDavid(800, 550);
+		Thread runner = new Thread(simon);
+		runner.start();
 	}
 
 	@Override
 	public void initScreen() {
-		// TODO Auto-generated method stub
+		SimonScreenDavid screen = new SimonScreenDavid(getWidth(), getHeight());
+		setScreen(screen);
 		
 	}
 
