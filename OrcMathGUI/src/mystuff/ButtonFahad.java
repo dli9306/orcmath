6817d4b9bedbfddd32cffa549854cc3ff2219df9
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-01-08 10:35:39 -0500

    classes created

---
+++ OrcMathGUI/src/mystuff/ButtonFahad.java
@@ -0,0 +1,43 @@
+package mystuff;
+
+import java.awt.Color;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class ButtonFahad extends Button implements ButtonInterfaceDavid {
+	
+	public static void main(String[] args) {
+		
+	}
+
+	public ButtonFahad(int x, int y, int w, int h, String text, Action action) {
+		super(x, y, w, h, "", null);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setColor(Color color) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void highlight() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dim() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getAButton() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OrcMathGUI/src/mystuff/MoveFahad.java
@@ -0,0 +1,21 @@
+package mystuff;
+
+public class MoveFahad implements MoveInterfaceDavid {
+
+	public MoveFahad() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ButtonInterfaceDavid getButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void getMove(int bIndex) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
