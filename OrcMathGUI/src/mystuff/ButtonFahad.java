96045e29b5e5e9ff4848090319ec4a5c753de0d2
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-01-08 11:44:13 -0500

    button edit

--- OrcMathGUI/src/mystuff/ButtonFahad.java
+++ OrcMathGUI/src/mystuff/ButtonFahad.java
@@ -1,21 +1,33 @@
 package mystuff;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 
 public class ButtonFahad extends Button implements ButtonInterfaceDavid {
 	
+	private static boolean buttonOn;
+	
 	public static void main(String[] args) {
-		
+		buttonOn = true;
 	}
 
 	public ButtonFahad(int x, int y, int w, int h, String text, Action action) {
 		super(x, y, w, h, "", null);
 		// TODO Auto-generated constructor stub
 	}
-
+	 
+	public void drawButton(Graphics2D g, boolean hover) {
+		if(buttonOn) {
+		
+		}
+		else {
+			 g.drawRect(getX(), getY(), getWidth(), getHeight());
+		}
+	}
+	
 	@Override
 	public void setColor(Color color) {
 		// TODO Auto-generated method stub
